1ad1f7ee444ca6bafdd4c82612cabc8f69b598b9
Randall Rouse <randall.g.rouse@gmail.com>
2022-01-26 09:44:47 -0500

    setup

--- db/index.js
+++ db/index.js
@@ -1,14 +1,21 @@
 const mongoose = require('mongoose')
-let MONGODB_URI = 'mongodb://127.0.0.1:27017/dogsDatabase'
+require('dotenv').config() // Add this line
+
+let dbUrl = process.env.NODE_ENV === 'production' ? process.env.MONGODB_URI : 'mongodb://127.0.0.1:27017/dogsDatabase'
+
 mongoose
-  .connect('mongodb://127.0.0.1:27017/dogsDatabase')
+  .connect(dbUrl, {
+    useUnifiedTopology: true,
+    useNewUrlParser: true,
+    useFindAndModify: true
+  })
   .then(() => {
     console.log('Successfully connected to MongoDB.')
   })
   .catch((e) => {
     console.error('Connection error', e.message)
   })
-// mongoose.set('debug', true)
+mongoose.set('debug', true)
 const db = mongoose.connection
 
 module.exports = db
